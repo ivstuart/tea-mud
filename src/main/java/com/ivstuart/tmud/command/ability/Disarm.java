aac43eb95b85e9beddbdc0cb65e85c98470f95ee
Ivan <Ivan.Stuart@gmail.com>
2016-09-01 17:49:32 +0100

    Added and tested out disarm

--- src/main/java/com/ivstuart/tmud/command/ability/Disarm.java
+++ src/main/java/com/ivstuart/tmud/command/ability/Disarm.java
@@ -21,72 +21,9 @@ import static com.ivstuart.tmud.constants.SkillNames.DISARM;
  */
 public class Disarm extends BaseCommand {
 
-	class FightActionBash extends FightAction {
-
-		public FightActionBash(Mob me, Mob target) {
-			super(me, target);
-		}
-
-		@Override
-		public void begin() {
-			super.begin();
-
-			// TODO review and test this
-			if (!getSelf().getMv().deduct(1)) {
-				out("You do not have enough movement left to disarm");
-				this.finished();
-			}
-
-			durationMillis(1000);
-			out(new Msg(getSelf(),getTarget(),("<S-You prepare your/NAME prepares GEN-him>self to disarm <T-you/NAME>.")));
-
-			if (checkMobStatus(getSelf(), getTarget())) {
-				this.finished();
-			}
-
-		}
-
-		@Override
-		public void changed() {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void ended() {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void happen() {
-			if (checkMobStatus(getSelf(), getTarget())) {
-				this.finished();
-			}
-
-			// Success or fail
-			Ability ability = getSelf().getLearned().getAbility(DISARM);
-			if (ability.isSuccessful()) {
-				out(new Msg(getSelf(),getTarget(),("<S-You/NAME> successfully disarmed <T-you/NAME>.")));
-				disarm(getSelf(), getTarget());
-				if (ability.isImproved()) {
-					out("[[[[ Your ability to " + ability.getId()
-							+ " has improved ]]]]");
-					ability.improve();
-				}
-			} else {
-				out(new Msg(getSelf(),getTarget(),("<S-You/NAME> miss<S-/es> disarming <T-you/NAME>.")));
-			}
-
-			durationMillis(1000);
-		}
-
-	}
-
 	private boolean checkMobStatus(Mob mob, Mob target) {
 
 		if (!mob.getState().canMove()) {
-			// You must be able to move to bash someone
 			mob.out("You must be standing or flying to disarm someone");
 			return true;
 		}
@@ -125,9 +62,9 @@ public class Disarm extends BaseCommand {
 
 	}
 
-	class FightActionBash extends FightAction {
+	class FightActionDisarm extends FightAction {
 
-		public FightActionBash(Mob me, Mob target) {
+		public FightActionDisarm(Mob me, Mob target) {
 			super(me, target);
 		}
 
@@ -185,7 +122,9 @@ public class Disarm extends BaseCommand {
 			durationMillis(1000);
 		}
 
-	}	@Override
+	}
+
+	@Override
 	public void execute(Mob mob, String input) {
 
 		if (!mob.getLearned().hasLearned(DISARM)) {
@@ -193,7 +132,14 @@ public class Disarm extends BaseCommand {
 			return;
 		}
 
-		Mob target = mob.getRoom().getMob(input);
+		Mob target;
+
+		if (input.length() == 0 && mob.getFight().isFighting()) {
+			target = mob.getFight().getTarget();
+		}
+		else {
+			target = mob.getRoom().getMob(input);
+		}
 
 		if (target == null) {
 			mob.out(input + " is not here to disarm!");
@@ -204,7 +150,7 @@ public class Disarm extends BaseCommand {
 			return;
 		}
 
-		mob.getFight().add(new FightActionBash(mob, target));
+		mob.getFight().add(new FightActionDisarm(mob, target));
 
 	}
 
